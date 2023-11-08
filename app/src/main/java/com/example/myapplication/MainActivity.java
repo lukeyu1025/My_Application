a7a9d9e10ef18eda0256197e5cf06d11948c3635
lukeyu1025 <lukeyu1025tw@yahoo.com>
2023-11-09 02:52:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    private MapView mapView;
+    private GoogleMap map; // Declare a member variable to hold the GoogleMap object
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap; // Assign the GoogleMap object to the member variable
+
+        LatLng location = new LatLng(25.05456, 121.590591);
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        map.addMarker(new MarkerOptions()
+                .position(location)
+                .title("Marker Title")
+                .snippet("Marker Description"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+}
